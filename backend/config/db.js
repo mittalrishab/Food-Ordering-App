d9e8be13617c386d12cda1719cfd68c1e7779ea9
Rishabh Mittal <rishabhmittalcoding@gmail.com>
2025-09-09 17:07:14 +0530

    updated security of config file

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,14 @@
 import mongoose from "mongoose";
+import 'dotenv/config';
 
-export const  connectDB = async () =>{
-
-    await mongoose.connect('mongodb+srv://mittalrishabh0305:rishabhdb@cluster0.tbio7pb.mongodb.net/food-del').then(()=>console.log("DB Connected"));
-   
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("DB Connected Successfully");
+  } catch (error) {
+    console.error("DB Connection Failed:", error);
+    process.exit(1); // Optional: exit the process if the DB connection fails
+  }
 }
-
-
 // add your mongoDB connection string above.
 // Do not use '@' symbol in your databse user's password else it will show an error.
